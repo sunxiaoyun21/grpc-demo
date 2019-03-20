fa21950edf1297d387a861c5fec75b0d4ce07898
sunxiaoyun <479338314@qq.com>
2019-03-20 22:37:53 +0800

    添加NSQ 功能

---
+++ grpcserver/src/main/java/nsq/NsqProducer.java
@@ -0,0 +1,35 @@
+package nsq;
+
+
+import com.github.brainlag.nsq.NSQProducer;
+import com.github.brainlag.nsq.exceptions.NSQException;
+
+import java.util.concurrent.TimeoutException;
+
+//生产者发布方法
+public class NsqProducer {
+
+
+    public  void  nsqProducer(){
+        NSQProducer producer=new NSQProducer();
+        //ip地址和端口号
+        producer.addAddress("localhost",8081).start();
+
+        try {
+            //名称，发布的消息
+            producer.produce("test","message".getBytes());
+        } catch (NSQException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+       NsqProducer nsqProducer=new NsqProducer();
+       nsqProducer.nsqProducer();
+    }
+
+
+
+}
